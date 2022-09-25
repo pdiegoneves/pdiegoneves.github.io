d5626c6d510bc20f4ee092cbcdb543435ee107e7
Diego Neves <pdcassiano@gmail.com>
2022-09-25 14:48:17 -0300

    criado os primeiros cards

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,12 @@
 const idadeText = document.getElementById('idade')
 data = new Date()
 idadeText.innerHTML = data.getFullYear() - 1986
+
+const projetos = document.querySelectorAll('#projetos article')
+
+projetos.forEach(projeto => {
+    projeto.addEventListener('click', (e) => {
+        e.preventDefault()
+        window.open(`/src/projetos/${projeto.id}/`, '_blank')
+    })
+})
