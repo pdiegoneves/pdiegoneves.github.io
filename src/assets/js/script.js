fad827f735df45591fe59558f61265c96292896a
Diego Neves <pdcassiano@gmail.com>
2022-09-26 06:23:53 -0300

    alterqação de layout, inicio de efeito

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,6 +3,7 @@ data = new Date()
 idadeText.innerHTML = data.getFullYear() - 1986
 
 const projetos = document.querySelectorAll('#projetos article')
+const topoDaPagina = document.getElementById('topo-da-pagina')
 
 projetos.forEach(projeto => {
     projeto.addEventListener('click', (e) => {
@@ -10,3 +11,22 @@ projetos.forEach(projeto => {
         window.open(`/src/projetos/${projeto.id}/`, '_blank')
     })
 })
+
+
+const sections = document.querySelectorAll('section')
+
+sections.forEach( (section) => {
+    section.style.paddingTop = topoDaPagina.clientHeight + 'px'
+})
+
+
+const efeitoDigitacao = document.querySelector('.efeito-digitacao')
+digitar()
+
+function digitar() {
+    frase = efeitoDigitacao.innerText.split('')
+    efeitoDigitacao.innerText = ''
+    efeitoDigitacao.innerHtml = ''
+    console.log(frase)
+
+}
