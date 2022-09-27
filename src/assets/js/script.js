6fa8fbcae72d4528aabdc7c63a55a7988a181f90
Diego Neves <pdcassiano@gmail.com>
2022-09-27 06:21:31 -0300

    efeito de digitação e mudança no menu

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,32 +1,56 @@
-const idadeText = document.getElementById('idade')
-data = new Date()
-idadeText.innerHTML = data.getFullYear() - 1986
+const idadeText = document.getElementById("idade");
+data = new Date();
+idadeText.innerHTML = data.getFullYear() - 1986;
 
-const projetos = document.querySelectorAll('#projetos article')
-const topoDaPagina = document.getElementById('topo-da-pagina')
-
-projetos.forEach(projeto => {
-    projeto.addEventListener('click', (e) => {
-        e.preventDefault()
-        window.open(`/src/projetos/${projeto.id}/`, '_blank')
-    })
-})
-
-
-const sections = document.querySelectorAll('section')
-
-sections.forEach( (section) => {
-    section.style.paddingTop = topoDaPagina.clientHeight + 'px'
-})
-
-
-const efeitoDigitacao = document.querySelector('.efeito-digitacao')
-digitar()
+const efeitoDigitacao = document.querySelector(".efeito-digitacao");
+digitar();
 
 function digitar() {
-    frase = efeitoDigitacao.innerText.split('')
-    efeitoDigitacao.innerText = ''
-    efeitoDigitacao.innerHtml = ''
-    console.log(frase)
+  const fraseInicial = efeitoDigitacao.innerText;
+  let fraseDigitando = "";
+  let indexParaCopiar = 0;
+  efeitoDigitacao.innerText = "";
+  const timer = setInterval(() => {
+    efeitoDigitacao.textContent = fraseDigitando;
+    if (fraseDigitando.length < fraseInicial.length) {
+      fraseDigitando = fraseInicial.slice(0, indexParaCopiar++);
+    } else {
+      clearInterval(timer);
+    }
+  }, 200);
+  console.log("frase inicial", fraseInicial);
+  console.log("frase digitando", fraseDigitando);
+}
 
+const sections = document.querySelectorAll("section");
+
+const projetos = document.querySelectorAll("#projetos article");
+const topoDaPagina = document.getElementById("topo-da-pagina");
+const menuTopo = document.querySelector("nav.menu-topo");
+
+document.addEventListener("scroll", () => {
+  alterarMenu();
+});
+
+sections.forEach((section) => {
+  section.style.paddingTop = topoDaPagina.clientHeight + "px";
+});
+
+projetos.forEach((projeto) => {
+  projeto.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.open(`/src/projetos/${projeto.id}/`, "_blank");
+  });
+});
+
+function alterarMenu() {
+  if (
+    sections[0].getBoundingClientRect().top +
+      sections[0].getBoundingClientRect().height <
+    0
+  ) {
+    menuTopo.classList.add("menuEscuro");
+  } else {
+    menuTopo.classList.remove("menuEscuro");
+  }
 }
