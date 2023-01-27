6bf5c441fc01c0b796d6f092111384b577da9760
Diego Neves <pdcassiano@gmail.com>
2023-01-27 20:46:38 -0300

    feat: initial layout

--- src/projetos/entendendo-transicoes-css/script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById('iniciarTransicao')
-pistas = document.querySelectorAll('.pista .bola')
-
-botao.addEventListener('click', () => {
-  pistas.forEach((pista) => {
-    pista.classList.toggle('go')
-  })
-})
