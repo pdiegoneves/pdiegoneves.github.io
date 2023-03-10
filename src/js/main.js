27c9b361e8a3843a0de1c1086663e79c9c6dad42
Diego Neves <pdcassiano@gmail.com>
2023-03-10 09:58:25 -0300

    mudança na chamada do conteúdo

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,25 @@
-const menuItens = document.querySelectorAll('.menu-item');
+const menuItens = document.querySelectorAll('.menu-item')
 
-menuItens.forEach(menuItem => {
-    menuItem.addEventListener('click', (e) => {
-        removeAllActives()
-        handleMenuItem(menuItem.getAttribute('data'))
-        menuItem.classList.add('active');
+menuItens.forEach((menuItem) => {
+  menuItem.addEventListener('click', (e) => {
+    removeAllActives()
+    handleMenuItem(menuItem.getAttribute('data'))
+    menuItem.classList.add('active')
+  })
+})
+
+async function handleMenuItem(page) {
+  const mainContent = document.querySelector('.main-content')
+  fetch(`src/pages/${page}.html`)
+    .then((response) => response.text())
+    .then((text) => {
+        mainContent.innerHTML = text
+        console.log(text)
     })
-}
-
-
-)
-
-function handleMenuItem(page) {
-    const mainContent = document.querySelector('.main-content');
-    mainContent.setAttribute('src', `src/pages/${page}.html`)
 }
 
 function removeAllActives() {
-    menuItens.forEach(menuItem => {
-        menuItem.classList.remove('active');
-    })
-
+  menuItens.forEach((menuItem) => {
+    menuItem.classList.remove('active')
+  })
 }
